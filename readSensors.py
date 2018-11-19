af2217ffce8e255d3433255e95f9a1772339c0bb
bradyjibanez <bradyjibanez@gmail.com>
2018-11-19 18:15:56 -0500

    brought back from the pymongo debacle 2018
    no more db clear, but user count differentiates users

--- readSensors.py
+++ readSensors.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 from mongoUpdate import Update
 import urllib, http.client, time, sys, Adafruit_DHT
+
 GPIO.setmode(GPIO.BOARD)
 
 ambientSensorIO = 15
@@ -20,25 +21,22 @@ def getLightReading(ambientSensorIO):
     while (GPIO.input(ambientSensorIO) == GPIO.LOW):
         chargeCount += 1
 
-#    GPIO.cleanup()
     return chargeCount
 
 def getTemperatureHumidity(temperatureSensorIO):
      humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-#     temp, hum = .format(temperature,humidity)
+
      return temperature, humidity
+
 try:
 
-#    GPIO.setup(temperatureSensor, GPIO.IN)
     useCount = 1
 
     while True:
         charge = getLightReading(ambientSensorIO)
         temperature, humidity = getTemperatureHumidity(temperatureSensorIO)
-#        humidity = str(str(humidity)+"%") use these if we want % and C in the mongoDB
-#        temperature = str(str(temperature)+"C")
 
-        if charge > 10000:
+        if charge > 1000:
             update.runUpdate(charge, ambientLightSensorData, useCount, temperature, humidity)
             print(charge, "green on")
             greenOff = urllib.request.urlopen("http://192.168.0.59/?cmd=TURN_ON_GREEN")
@@ -48,12 +46,11 @@ try:
             print(charge, "green off")
             greenOn = urllib.request.urlopen("http://192.168.0.59/?cmd=TURN_OFF_GREEN")
             useCount += 1
-#        humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-#        print('Temp: {0:0.1f} C Humidity: {1:0.1f} %'.format(temperature,humidity))
-#        print(temperature, humidity)
+        humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+        print('Temp: {0:0.1f} C Humidity: {1:0.1f} %'.format(temperature,humidity))
 except KeyboardInterrupt:
     pass
-#except httplib.BadStatusLine:
-#    checker = rc_time(pin_to_circuit)
+
+
 finally:
     GPIO.cleanup()
