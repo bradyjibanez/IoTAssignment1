4dbbd064e0beb5573f138579bfab249bec62716b
bradyjibanez <bradyjibanez@gmail.com>
2018-11-19 18:39:16 -0500

    Back to life

--- readSensors.py
+++ readSensors.py
@@ -40,12 +40,12 @@ try:
 
         if charge > 1000:
             update.runUpdate(charge, ambientLightSensorData, useCount, temperature, humidity)
-            print("Light level: ", charge, "Light status: green on")
+            print("Light level:", charge, "Light status: green on")
             urllib.request.urlopen("http://192.168.0.59/?cmd=TURN_ON_GREEN")
             useCount += 1
         if charge < 1000:
             update.runUpdate(charge, ambientLightSensorData, useCount, temperature, humidity)
-            print("Light level: ", charge, "Light status: green off")
+            print("Light level:", charge, "Light status: green off")
             urllib.request.urlopen("http://192.168.0.59/?cmd=TURN_OFF_GREEN")
             useCount += 1
         humidity, temperature = Adafruit_DHT.read_retry(11, 4)
